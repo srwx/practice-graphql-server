ce58b3d52391489f7c416e2bf1674cff7f69f7dd
Sorawong L <Sorawong2543@gmail.com>
2022-12-11 13:50:53 +0700

    init prettier config

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express"
+import { graphqlHTTP } from "express-graphql" // allow express to understand graphql
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
+const port = process.env.PORT
 
-const app: Express = express();
-const port = process.env.PORT;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const app: Express = express()
+app.use("/graphql", graphqlHTTP)
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
