4de7b2d18746c1c96e5944655a029d8ff21ab7d3
Sorawong L <Sorawong2543@gmail.com>
2022-12-12 23:50:32 +0700

    init `BookType`

---
+++ schema/types/Book.ts
@@ -0,0 +1,10 @@
+import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql"
+
+export const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+})
