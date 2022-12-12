edca974d6216145297cb3db8dd5ea16f300b4016
Sorawong L <Sorawong2543@gmail.com>
2022-12-13 00:07:16 +0700

    used `GraphQLID` as a book's id type instead of `GraphQLString`

--- schema/queries/Book.ts
+++ schema/queries/Book.ts
@@ -1,11 +1,11 @@
 import { tempBook } from "db/book"
-import { GraphQLString } from "graphql"
+import { GraphQLID } from "graphql"
 import { BookType } from "schema/types/Book"
 
 export const GET_BOOK = {
   type: BookType,
   args: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
   },
   resolve(parent: any, args: { id: string }) {
     return tempBook.find((book) => book.id === args.id)
