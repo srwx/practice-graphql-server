d13cc5ee9b1de42a251676c48b2d85ca017b2cb3
Sorawong L <Sorawong2543@gmail.com>
2022-12-12 23:50:16 +0700

    init `/graphql` middleware

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
-import express, { Express, Request, Response } from "express"
+import express, { Express } from "express"
 import { graphqlHTTP } from "express-graphql" // allow express to understand graphql
 import dotenv from "dotenv"
+import { schema } from "schema"
 
 dotenv.config()
 const port = process.env.PORT
 
 const app: Express = express()
-app.use("/graphql", graphqlHTTP)
+
+// middleware for '/graphql'
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
