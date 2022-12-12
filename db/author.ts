cfc5eb6f5b337aaa3c570472227cfaadb90765e1
Sorawong L <Sorawong2543@gmail.com>
2022-12-13 00:14:26 +0700

    init temp author database

---
+++ db/author.ts
@@ -0,0 +1,5 @@
+export const tempAuthor = [
+  { id: "1", name: "Patrick Rothfuss", age: 44 },
+  { id: "2", name: "Brandon Sanderson", age: 42 },
+  { id: "3", name: "Terry Pratchett", age: 66 },
+]
