226190c03a3cab3543275e01ab3a5a661704898c
Sorawong L <Sorawong2543@gmail.com>
2022-12-13 22:30:16 +0700

    add relation `book` in author type

--- schema/types/Author.ts
+++ schema/types/Author.ts
@@ -1,15 +1,25 @@
+import { tempBook } from "db/book"
 import {
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
   GraphQLObjectType,
   GraphQLString,
 } from "graphql"
+import { BookType } from "./Book"
 
+// @ts-ignore
 export const AuthorType = new GraphQLObjectType({
   name: "Author",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent: any, _) {
+        return tempBook.filter((book) => book.authorId === parent.id)
+      },
+    },
   }),
 })
