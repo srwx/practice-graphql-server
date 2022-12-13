c214f510c82e8ad4ebc4fd6b124e22606092a54a
Sorawong L <Sorawong2543@gmail.com>
2022-12-13 21:50:57 +0700

    add relation type

--- db/book.ts
+++ db/book.ts
@@ -3,15 +3,18 @@ export const tempBook = [
     id: "1",
     name: "Name of the Wind",
     genre: "Fantasy",
+    authorId: "1",
   },
   {
     id: "2",
     name: "The Final Empire",
     genre: "Fantasy",
+    authorId: "2",
   },
   {
     id: "3",
     name: "The Long Earth",
     genre: "Sci-Fi",
+    authorId: "3",
   },
 ]
--- schema/types/Book.ts
+++ schema/types/Book.ts
@@ -1,4 +1,6 @@
+import { tempAuthor } from "db/author"
 import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql"
+import { AuthorType } from "./Author"
 
 export const BookType = new GraphQLObjectType({
   name: "Book",
@@ -6,5 +8,13 @@ export const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    authorId: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent: any, args: any) {
+        // parent: book ที่ถูก query
+        return tempAuthor.find((author) => author.id === parent.authorId)
+      },
+    },
   }),
 })
