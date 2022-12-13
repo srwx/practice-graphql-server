c95b024175a738196228b093513c8b267d1d3d2e
Sorawong L <Sorawong2543@gmail.com>
2022-12-13 22:05:28 +0700

    update temp book db

--- db/book.ts
+++ db/book.ts
@@ -1,20 +1,8 @@
 export const tempBook = [
-  {
-    id: "1",
-    name: "Name of the Wind",
-    genre: "Fantasy",
-    authorId: "1",
-  },
-  {
-    id: "2",
-    name: "The Final Empire",
-    genre: "Fantasy",
-    authorId: "2",
-  },
-  {
-    id: "3",
-    name: "The Long Earth",
-    genre: "Sci-Fi",
-    authorId: "3",
-  },
+  { id: "1", name: "Name of the Wind", genre: "Fantasy", authorId: "1" },
+  { id: "2", name: "The Final Empire", genre: "Fantasy", authorId: "2" },
+  { id: "3", name: "The Long Earth", genre: "Sci-Fi", authorId: "3" },
+  { id: "4", name: "The Hero of Ages", genre: "Fantasy", authorId: "2" },
+  { id: "5", name: "The Colour of Magic", genre: "Fantasy", authorId: "3" },
+  { id: "6", name: "The Light Fantastic", genre: "Sci-Fi", authorId: "3" },
 ]
